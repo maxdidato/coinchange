6363481a1daa975e43e80b005331dba517c13c87
mdidato <d>
2017-09-03 17:00:19 +0100

    feat(): add further test on the Coin object to ensure the denomination of each coin is correct

--- src/test/java/com/maxdidato/coinchange/CoinTest.java
+++ src/test/java/com/maxdidato/coinchange/CoinTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.security.InvalidParameterException;
 import java.util.Arrays;
 
+import static com.maxdidato.coinchange.Coin.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -23,5 +24,38 @@ public class CoinTest {
         Coin.valueOf(12345);
     }
 
+    @Test
+    public void the_denomination_of_one_pound_is_100() {
+        assertThat(ONE_POUND.getDenomination(), is(100));
+    }
 
+    @Test
+    public void the_denomination_of_fifty_pence_is_50() {
+        assertThat(FIFTY_PENCE.getDenomination(), is(50));
+    }
+
+    @Test
+    public void the_denomination_of_twenty_pence_is_20() {
+        assertThat(TWENTY_PENCE.getDenomination(), is(20));
+    }
+
+    @Test
+    public void the_denomination_of_ten_pence_is_10() {
+        assertThat(TEN_PENCE.getDenomination(), is(10));
+    }
+
+    @Test
+    public void the_denomination_of_five_pence_is_5() {
+        assertThat(FIVE_PENCE.getDenomination(), is(5));
+    }
+
+    @Test
+    public void the_denomination_of_two_pence_is_2() {
+        assertThat(TWO_PENCE.getDenomination(), is(2));
+    }
+
+    @Test
+    public void the_denomination_of_one_penny_is_1() {
+        assertThat(ONE_PENNY.getDenomination(), is(1));
+    }
 }
